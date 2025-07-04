ca5a6a2630cb677d1b9d9a424fca0f7cf669eb18
champagne101 <99389926+champagne101@users.noreply.github.com>
2025-07-04 11:47:27 +0200

    Add files via upload

---
+++ frontend/pages/index.js
@@ -0,0 +1,33 @@
+import Script from "next/script";
+import Interface from '../components/interface';
+
+const index = () => {
+  return (
+    <div>
+       {/** <link rel="stylesheet" href="/css/bootstrap.min.css"></link> */}
+      <Script src="/js/snarkjs.min.js" />
+      <Interface />
+    </div>
+    
+  )
+}
+
+export default index
+
+
+/*
+import Script from "next/script";
+import Interface from '../components/Interface.js';
+
+const Index = () => {
+    return (
+        <div>
+            <link rel="stylesheet" href="/css/bootstrap.min.css"></link>
+            <Script src="/js/snarkjs.min.js" />
+            <Interface />
+        </div>
+    )
+};
+
+export default Index;
+*/
